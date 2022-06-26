162731cbacca40bbf378c3a6e9e46331d7ec6df4
IvanProkhorets <prohorets.ivan@gmail.com>
2022-06-26 22:30:06 +0300

    Initial commit

---
+++ _optimized_gulp_sass_jade/gulpfile.js
@@ -0,0 +1,71 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass')(require('sass')),
+		autoprefixer = require('gulp-autoprefixer'),
+		minifycss    = require('gulp-minify-css'),
+		rename       = require('gulp-rename'),
+		browserSync  = require('browser-sync').create(),
+		jade         = require('gulp-jade'),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify-es').default;
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// online: false, // Work offline without internet connection
+		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+function bsReload(done) { browserSync.reload(); done() };
+
+gulp.task('styles', function () {
+	return gulp.src('sass/*.sass')
+	.pipe(sass({
+		includePaths: require('node-bourbon').includePaths
+	}).on('error', sass.logError))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer({
+		// grid: true, // Optional. Enable CSS Grid
+		overrideBrowserslist: ['last 10 versions']
+	}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('jade', function() {
+	return gulp.src('jade/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest('app'))
+	.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/**/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'./app/libs/modernizr/modernizr.js',
+		'./app/libs/jquery/jquery-1.11.2.min.js',
+		'./app/libs/waypoints/waypoints.min.js',
+		'./app/libs/animate/animate-css.js',
+		'./app/libs/plugins-scroll/plugins-scroll.js',
+		])
+		.pipe(concat('libs.js'))
+		// .pipe(uglify()) //Minify libs.js
+		.pipe(gulp.dest('./app/js/'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('sass/*.sass', gulp.parallel('styles'));
+	gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('scripts'));
+	gulp.watch('jade/*.jade', gulp.parallel('jade'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'scripts', 'jade', 'browser-sync', 'watch'));
